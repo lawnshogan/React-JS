515264bb07e7e696a5a9a6d8e10b663d63eeafd4
lawnshogan <Shawnlogan.gis@gmail.com>
2022-10-17 10:13:46 -0600

    React Adds

--- shawns_app/src/components/ExpenseItem.js
+++ shawns_app/src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
 import './ExpenseItem.css';
-function ExpenseItem() {  
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
-
+function ExpenseItem(propData) {  
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{propData.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{propData.title}</h2>
+        <div className="expense-item__price">${propData.amount}</div>
       </div>
     </div>
   );
