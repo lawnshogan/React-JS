3f10a364395f8265453c38d444e150706d464c79
lawnshogan <Shawnlogan.gis@gmail.com>
2022-11-19 14:32:11 -0700

    practice git

--- 4_React_State_Events/code/09-finished/src/components/Expenses/ExpensesFilter.js
+++ 4_React_State_Events/code/09-finished/src/components/Expenses/ExpensesFilter.js
@@ -6,7 +6,7 @@ const ExpensesFilter = (props) => {
   const dropdownChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
   };
-
+more
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
